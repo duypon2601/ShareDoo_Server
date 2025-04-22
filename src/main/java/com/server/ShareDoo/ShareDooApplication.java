1343f63a312e5ce73c729c225d86496466947058
duypo <duytntse171996@fpt.edu.vn>
2025-04-22 08:32:48 +0700

    'firstcommit''

---
+++ src/main/java/com/server/ShareDoo/ShareDooApplication.java
@@ -0,0 +1,13 @@
+package com.server.ShareDoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareDooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareDooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/ShareDoo/ShareDooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.ShareDoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareDooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
