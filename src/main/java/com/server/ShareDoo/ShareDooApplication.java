14b2d82b11f8c34967b49598a07286f007ecc101
duypo <duytntse171996@fpt.edu.vn>
2025-06-19 18:30:39 +0700

    AI Agent Integration 51%

--- src/main/java/com/server/ShareDoo/ShareDooApplication.java
+++ src/main/java/com/server/ShareDoo/ShareDooApplication.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info( version = "2.0", description = "Information"))
 @SecurityScheme(name = "api", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER, bearerFormat = "JWT")
